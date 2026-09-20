12dc1da953e2db4fa3c4c01d0a39b33116c1f29f
Harsh Raj Singhania <harshrajsinghania@proton.me>
2026-09-20 10:05:08 +0530

    Avoid deprecated datetime.utcnow() in PointSet test metadata

--- tests/test_pointset.py
+++ tests/test_pointset.py
@@ -13,7 +13,7 @@ def test_pointset():
     assert elem.location_length("vertices") == 10
     elem.metadata = {
         "color": "green",
-        "date_created": datetime.datetime.utcnow(),
+        "date_created": datetime.datetime.now(datetime.timezone.utc).replace(tzinfo=None),
         "version": "v1.3",
     }
     assert elem.validate()
